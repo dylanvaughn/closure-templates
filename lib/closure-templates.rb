4bb07b545f3638573e806cfd60569cd8a04aa5d5
Dylan Vaughn <dylan.vaughn@stlondemand.com>
2011-11-04 12:03:23 -0700

    Adding more tests and some error checking

--- lib/closure-templates.rb
+++ lib/closure-templates.rb
@@ -9,15 +9,20 @@ class ClosureTemplates
   @@files = {}
   @@template_dir = nil
   @@tofu = nil
+  @@initialized = false
   
   def self.init(dir)
     @@template_dir = dir
+    @@initialized = true
     self.compile
   end
 
   def self.render(template, assigns = {})
-    if @@tofu.nil?
+    if !@@initialized
       raise "ERROR: Not initialized!\nInitialize by calling 'ClosureTemplates.init(path_to_templates)' before calling render.\n"
+    end
+    if @@tofu.nil?
+      raise "ERROR: No templates found in #{@@template_dir}"
     end
     @@files.keys.each do |f|
       if File.mtime(f).to_i > @@files[f]
@@ -25,7 +30,6 @@ class ClosureTemplates
         break
       end
     end
-    puts "banana: rendering!"
     locals = assigns.dup
     locals.keys.each do |key|
       locals[key.to_s] = locals.delete(key)
@@ -34,15 +38,19 @@ class ClosureTemplates
   end
 
   def self.compile
-    puts "banana: compiling!"
     @@files = {}
     sfs_builder = SoyFileSet::Builder.new
     Dir.glob("#{@@template_dir}/**/*.soy") do |file|
       @@files[file] = File.mtime(file).to_i
       sfs_builder.add(java.io.File.new(file))
     end
-    sfs = sfs_builder.build
-    @@tofu = sfs.compileToJavaObj
+    if @@files.keys.size == 0
+      # No templates
+      @@tofu = nil
+    else
+      sfs = sfs_builder.build
+      @@tofu = sfs.compileToJavaObj
+    end
   end
 
 end
--- test/test_closure-templates.rb
+++ test/test_closure-templates.rb
@@ -2,8 +2,42 @@ require 'test/unit'
 require 'closure-templates'
 
 class TestClosureTemplates < Test::Unit::TestCase
+
   def test_simple_template
     ClosureTemplates.init("#{File.dirname(__FILE__)}/templates")
     assert_equal ClosureTemplates.render('examples.simple.helloSimple', { :name => 'Bob' }), 'Hiya Bob!'
   end
+
+  def test_more_complicated_template
+    ClosureTemplates.init("#{File.dirname(__FILE__)}/templates")
+    assert_equal ClosureTemplates.render('examples.simple.hello', { :name => 'Jane', :additionalNames => ['Sue', 'Frank'] }), 'Hello Jane!<br>Hello Sue!<br>Hello Frank!'
+  end
+
+  def test_simple_template_without_locals
+    assert_raise NativeException do
+      ClosureTemplates.init("#{File.dirname(__FILE__)}/templates")
+      ClosureTemplates.render('examples.simple.helloSimple')
+    end
+  end
+
+  def test_empty_template_dir
+    assert_nothing_raised do
+      ClosureTemplates.init("#{File.dirname(__FILE__)}/no_templates")
+    end
+  end
+
+  def test_calling_render_with_no_templates
+    assert_raise RuntimeError do
+      ClosureTemplates.init("#{File.dirname(__FILE__)}/no_templates")
+      ClosureTemplates.render('examples.simple.helloSimple')
+    end
+  end
+
+  def test_calling_render_with_incorrect_template_name
+    assert_raise NativeException do
+      ClosureTemplates.init("#{File.dirname(__FILE__)}/templates")
+      ClosureTemplates.render('examples.simple.notSoSimple', { :name => 'Bob' })
+    end
+  end
+
 end
